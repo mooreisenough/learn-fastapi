76a7956eb20f322bfa6c0532eb86401085c48db4
George Moore <mooreisenough@netscape.net>
2023-07-09 09:14:55 +0000

    Validated yoyo migration tool

---
+++ lesson1/config/dev/migrations/fastapi_202307090836.sql
@@ -0,0 +1,14 @@
+CREATE TABLE
+    IF NOT EXISTS "newbie.textsummary" (
+        "id" SERIAL NOT NULL PRIMARY KEY,
+        "url" TEXT NOT NULL,
+        "summary" TEXT NOT NULL,
+        "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+    );
+
+CREATE TABLE
+    IF NOT EXISTS "newbie.aerich" (
+        "id" SERIAL NOT NULL PRIMARY KEY,
+        "version" VARCHAR(255) NOT NULL,
+        "app" VARCHAR(100) NOT NULL,
+        "content" JSONB NOT NULL);
---
+++ lesson1/config/migrations/fastapi_202307090822.sql
@@ -0,0 +1,3 @@
+-- transactional: false
+CREATE DATABASE web_dev;
+CREATE DATABASE web_test;
